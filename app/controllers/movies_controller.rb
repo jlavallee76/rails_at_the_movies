d94097ad04de2e7b342ff9513f1b3e5a07a2b658
Josh Lavallee <jlavallee76@academic.rrc.ca>
2021-02-04 14:45:08 -0600

    Added Bulma, Routes,  and Controllers

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,11 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.includes(:production_company)
+                   .all
+                   .order("average_vote DESC")
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+end
